705f251c8f3f3f2b0983be50aba187d6804193ab
AngelaKoshel <angela65784@gmail.com>
2023-07-19 15:33:48 +0300

    add

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formElem = document.querySelector(".login-form");
+formElem.addEventListener("submit", fooSubmit);
 
+function fooSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields");
+    }
+    const objResult = {};
+    objResult.Email = email.value;
+    objResult.Password = password.value;
+    console.log(objResult);
+
+    event.currentTarget.reset();
+}
