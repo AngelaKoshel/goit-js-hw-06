0e8c3a5ccc5582c420e9636dd0bb42b61574d2a1
AngelaKoshel <angela65784@gmail.com>
2023-07-26 17:16:55 +0300

    add

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,9 @@ const spanElement = document.querySelector("#name-output");
 const defaultName = spanElement.textContent;
 
 inputElement.addEventListener("input", (event) => {
-    if (inputElement.value === "" ) {
+    if (inputElement.value.trim() === "" ) {
         spanElement.textContent = defaultName;
     } else {
-        spanElement.textContent = event.currentTarget.value;
+        spanElement.textContent = event.currentTarget.value.trim();
     }
 } );
--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const requiredLength = Number(inputElem.getAttribute("data-length"));
 inputElem.addEventListener("blur", onInputBlur);
 function onInputBlur(event) {
   inputElem.classList.remove("valid", "invalid");
-  if (requiredLength === event.currentTarget.value.length) {
+  if (requiredLength === event.currentTarget.value.trim().length) {
     inputElem.classList.add("valid");
   } else {
     inputElem.classList.add("invalid");
